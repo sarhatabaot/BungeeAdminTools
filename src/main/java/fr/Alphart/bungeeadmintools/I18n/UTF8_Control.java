5f7a149cb1dbf92b68ceba69ab79c3e129e41245
Omer Oreg <sarhatabaot1@gmail.com>
2021-05-28 17:24:03 +0300

    Removed BP

--- src/main/java/fr/Alphart/bungeeadmintools/I18n/UTF8_Control.java
+++ src/main/java/fr/Alphart/bungeeadmintools/I18n/UTF8_Control.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
@@ -16,7 +17,7 @@ public class UTF8_Control extends Control {
 			final ClassLoader loader, final boolean reload) throws IOException {
 		final String bundleName = toBundleName(baseName, locale);
 		final String resourceName = toResourceName(bundleName, "language");
-		ResourceBundle bundle = null;
+
 		InputStream stream = null;
 		if (reload) {
 			final URL url = loader.getResource(resourceName);
@@ -30,9 +31,10 @@ public class UTF8_Control extends Control {
 		} else {
 			stream = loader.getResourceAsStream(resourceName);
 		}
+		ResourceBundle bundle = null;
 		if (stream != null) {
 			try {
-				bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+				bundle = new PropertyResourceBundle(new InputStreamReader(stream, StandardCharsets.UTF_8));
 			} finally {
 				stream.close();
 			}
