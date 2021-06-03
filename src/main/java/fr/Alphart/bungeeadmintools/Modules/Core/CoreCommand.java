827cb35a83cd3cf966a53de6d9a3d063f2d0c64c
Omer Oreg <sarhatabaot1@gmail.com>
2021-06-03 22:05:14 +0300

    Added reload command

--- src/main/java/fr/Alphart/bungeeadmintools/Modules/Core/CoreCommand.java
+++ src/main/java/fr/Alphart/bungeeadmintools/Modules/Core/CoreCommand.java
@@ -5,37 +5,52 @@ import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.HelpCommand;
 import co.aikar.commands.annotation.Subcommand;
+import fr.alphart.bungeeadmintools.BungeeAdminToolsPlugin;
+import fr.alphart.bungeeadmintools.I18n.I18n;
+import fr.alphart.bungeeadmintools.Permissions;
+import net.cubespace.Yamler.Config.InvalidConfigurationException;
+import net.md_5.bungee.api.CommandSender;
 
-//TODO
+import static fr.alphart.bungeeadmintools.I18n.I18n.formatWithColorAndAddPrefix;
+
 @CommandAlias("bat")
 public class CoreCommand extends BaseCommand {
 
     @Subcommand("reload")
-    @CommandPermission("bat.reload")
-    public void onReload() {
-
+    @CommandPermission(Permissions.RELOAD)
+    public void onReload(final CommandSender sender) {
+        try {
+            BungeeAdminToolsPlugin.getInstance().getConfiguration().reload();
+        } catch (InvalidConfigurationException e) {
+            BungeeAdminToolsPlugin.getInstance().getLogger().severe("Error during reload of main configuration :");
+            e.printStackTrace();
+        }
+        I18n.reload();
+        BungeeAdminToolsPlugin.getInstance().getModules().unloadModules();
+        BungeeAdminToolsPlugin.getInstance().getModules().loadModules();
+        sender.sendMessage(formatWithColorAndAddPrefix("Reload successfully executed ..."));
     }
 
     @Subcommand("modules")
-    @CommandPermission("bat.modules")
+    @CommandPermission(Permissions.MODULES)
     public void onModules() {
 
     }
 
     @Subcommand("version")
-    @CommandPermission("bat.version")
+    @CommandPermission(Permissions.VERSION)
     public void onVersion() {
 
     }
 
     @Subcommand("backup")
-    @CommandPermission("bat.backup")
+    @CommandPermission(Permissions.BACKUP)
     public void onBackup() {
 
     }
 
     @HelpCommand
-    @CommandPermission("bat.help")
+    @CommandPermission(Permissions.HELP)
     public void onHelp(){
 
 
